c3de1f31861598e4d486915c8f2383ff3b9b430e
lazarev21 <lazarevg@bk.ru>
2022-04-06 16:25:47 +0300

    added npx, esl

--- js/consts.js
+++ js/consts.js
@@ -1,19 +1,19 @@
 export {SERVER,
-        favoriteCityList,
         form,
         likeIcon,
         tabs,
         errorDublicationCity,
-
     }
 
 const SERVER = {
     URL: 'http://api.openweathermap.org/data/2.5/forecast',
     URL_ICONS: 'http://openweathermap.org/img/w/',
     API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f',
+    DEFAULT_CITY: 'Moscow',
 };
 
-const favoriteCityList = document.querySelector('.main__list');
+
+//const favoriteCityList = document.querySelector('.main__list');
 const form = document.getElementById('form');
 const likeIcon = document.querySelector('.main__heart');
 const tabs = document.querySelector('.main__box-tabs');
